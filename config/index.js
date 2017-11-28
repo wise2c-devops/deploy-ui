18e49de99272a35e1602a1885acbd5782fe6ffa7
wsyWeb <823391837@qq.com>
2017-11-28 17:37:53 +0800

    merge

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 4000,
+    port: 3000,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
