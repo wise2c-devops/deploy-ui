587a9fe8e8075fc11b453f3266ae785424b86305
wsyWeb <823391837@qq.com>
2017-11-29 12:00:04 +0800

    merge

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   WEBSOCKET_HOST: '"ws://172.20.3.1:8080"',
-  HTTP_HOST: '"http://172.20.3.1:8080"',
+  HTTP_HOST: '"http://114.55.127.173:88"'
 })
