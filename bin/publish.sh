995f01d6d2cdf69aa4185824a8213b30a1ac797f
wsyWeb <823391837@qq.com>
2018-12-23 19:29:42 +0800

    q清空登录账号密码

--- bin/publish.sh
+++ bin/publish.sh
@@ -11,6 +11,6 @@ cd $ROOT_PATH
 cnpm install
 npm run build
 docker build --build-arg buildInfo=$(date +'%Y%m%d-%H%M%S') -t registry.cn-hangzhou.aliyuncs.com/wise2c-dev/deploy-ui:$VERSION .
-docker login -u="wise2cdev" -p="Wise2c2017" registry.cn-hangzhou.aliyuncs.com
+docker login -u="" -p="" registry.cn-hangzhou.aliyuncs.com
 docker push registry.cn-hangzhou.aliyuncs.com/wise2c-dev/deploy-ui:$VERSION
 echo "===========SUCCESS=========="
