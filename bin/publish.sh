7c3e00787f9f26f861bb42bf7423a10fad70b1d6
wsyWeb <823391837@qq.com>
2018-12-23 19:33:20 +0800

    merge

--- bin/publish.sh
+++ bin/publish.sh
@@ -10,7 +10,7 @@ echo "PUBLISH VERSION"$VERSION
 cd $ROOT_PATH
 cnpm install
 npm run build
-docker build --build-arg buildInfo=$(date +'%Y%m%d-%H%M%S') -t registry.cn-hangzhou.aliyuncs.com/wise2c-dev/deploy-ui:$VERSION .
-docker login -u="" -p="" registry.cn-hangzhou.aliyuncs.com
-docker push registry.cn-hangzhou.aliyuncs.com/wise2c-dev/deploy-ui:$VERSION
+docker build --build-arg buildInfo=$(date +'%Y%m%d-%H%M%S') -t wise2c/deploy-ui:$TRAVIS_BRANCH .
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push wise2c/deploy-ui:$TRAVIS_BRANCH
 echo "===========SUCCESS=========="
