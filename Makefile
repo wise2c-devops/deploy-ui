e382f2e3725533f7eda31717084b1de92e41715c
wsyWeb <823391837@qq.com>
2018-12-29 16:25:21 +0800

    merge

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 REPO=registry.cn-hangzhou.aliyuncs.com/wise2c-dev/deploy-ui
 VERSION=latest
-DEV_VERSION=v0.5.2
+DEV_VERSION=v0.5.5
 
 .PHONY:build push
 build:
@@ -8,9 +8,10 @@ build:
 
 push:
 	docker tag $(REPO):$(VERSION) $(REPO):$(DEV_VERSION)
-	docker push $(REPO):$(VERSION)
+	# docker push $(REPO):$(VERSION)
 	docker push $(REPO):$(DEV_VERSION)
 
 run:
 	-docker rm -f deploy-ui
 	docker run -d --name deploy-ui -p 3000:80 $(REPO):$(VERSION)
+
--- bin/publish.sh
+++ bin/publish.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-echo "============================="
-SCRIPT_PATH=$(cd `dirname $0`; pwd)
-echo "scriptPath: "$SCRIPT_PATH
-ROOT_PATH=$(dirname $SCRIPT_PATH)
-echo "WORKSPACE:"$ROOT_PATH
-VERSION=$1
-echo "PUBLISH VERSION"$VERSION
-
 cd $ROOT_PATH
 cnpm install
 npm run build
-docker build --build-arg buildInfo=$(date +'%Y%m%d-%H%M%S') -t wise2c/deploy-ui:$TRAVIS_BRANCH .
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push wise2c/deploy-ui:$TRAVIS_BRANCH
+docker build --build-arg buildInfo=$(date +'%Y%m%d-%H%M%S') -t registry.cn-hangzhou.aliyuncs.com/wise2c-dev/deploy-ui:$VERSION .
+docker login -u="wise2cdev" -p="Wise2c2017" registry.cn-hangzhou.aliyuncs.com
+# echo "wise2c2017" | docker login -u="wise2cdev" registry.cn-hangzhou.aliyuncs.com
+docker push registry.cn-hangzhou.aliyuncs.com/wise2c-dev/deploy-ui:$VERSION
 echo "===========SUCCESS=========="
