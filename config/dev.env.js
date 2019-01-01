116622596071052f361672e321239d8fd04816e8
Alan Peng <peng.alan@gmail.com>
2018-12-31 23:26:02 -0500

    Updated env configuration

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  WEBSOCKET_HOST: '"ws://172.20.3.1:8080"',
-  HTTP_HOST: '"http://111.231.216.74:88"'
+  WEBSOCKET_HOST: '"ws://192.168.9.10:8080"',
+  HTTP_HOST: '"http://192.168.9.10:88"'
 })
