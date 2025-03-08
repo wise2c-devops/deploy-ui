9227cec79c7929bf86d4ad88997f8f7f431b5e20
Alan Peng <peng.alan@gmail.com>
2025-03-08 09:49:20 +0800

    Update build file

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -37,6 +37,18 @@ var webpackConfig = merge(baseWebpackConfig, {
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
   plugins: [
     // http://vuejs.github.io/vue-loader/workflow/production.html
     new webpack.DefinePlugin({
@@ -72,25 +84,10 @@ var webpackConfig = merge(baseWebpackConfig, {
       },
       scriptLoading: 'defer'
     }),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
+    new webpack.optimize.RuntimeChunkPlugin({
+      name: 'manifest'
     }),
     new webpack.ProvidePlugin({
       $: "jquery",
